be151bdc97145b44e3c66bc0931066d99a398337
sam <samdhanaseelan2636.com>
2022-07-02 13:30:55 +0530

    adding data

---
+++ salaray_prob.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df=pd.read_csv("SalaryGender.csv",delimiter=',')
+
+data=df.corr()
+sns.heatmap(data,square=True,cmap="bwr")
+plt.yticks(rotation=0)
+plt.xticks(rotation=90)
+#plt.show()
+#data exploring---------------------->
+#print(df)
+print(df.head())
+print(df.info())
+print(df.describe().round(2))   #describe() is used to get the statistical information of the dataframe
+salary=np.array(df['Salary'])
+
+gender=np.array(df['Gender'])  #type casting 
+phd=np.array(df['PhD'])
+age=np.array(df['Age'])
+
+age1=df['Age'].values   #prefferd way of doing
+age2=df['Age'].dtype  #prefferd way of doing
+print(type(age1))
+print(age2)
+
+#to check no of rows and cloumns
+print(df.shape)
+
+#to fetch the multiple columns
+df_fetch=df[['Salary','Age']]   #provide list of the column names
+
+print(df_fetch)
+
+#fetching a row 
+fetch_row1=df.loc[0:10]  #loc gives 0 to 10 rows
+fetch_row2=df.iloc[0:10,0:2]  #iloc gives ddta 0 to 9 rows   i represents indxed location
+print(fetch_row2)
---
+++ school_exploration.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns 
+
+df=pd.read_csv("middle_tn_schools.csv",delimiter=',')
+
+
+#data exploring---------------------->using heatmap sns.heatmap(data,square=True,cmap="bwr")
+data=df.corr()
+sns.heatmap(data,square=True,annot=True,cmap="bwr")
+plt.yticks(rotation=0)
+plt.xticks(rotation=90)
+#plt.show()   #to show the heatmap
+
+#data ifo and describe()
+print(df.head())
+print(df.tail())
+print(df.info())
+print(df.describe().round(2))
+print(df['name'].values)
+print(df['name'].dtypes)
+print(df.shape)
+
+#to check the corellation of reduced_lunch and school_ratinngs
+data2=df[['reduced_lunch','school_rating']]
+school_data=data2.corr()
+sns.heatmap(school_data,square=True,annot=True,cmap="bwr")
+plt.yticks(rotation=0)
+plt.xticks(rotation=90)
+plt.show()
